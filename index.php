dd88be477611eb74f49f7c2d2b59d2fa2bf01ff2
Mozhgan Moghanian <mom698@g.harvard.edu>
2017-02-09 05:43:17 -0800

    error handeling, doctype html4

--- index.php
+++ index.php
@@ -28,8 +28,10 @@ $quoteString = $selectedQuote['quote'];
 $quoteSource = $selectedQuote['author'];
 
 ?>
-<!DOCTYPE html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
 <html>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<head>
 		<title>Mozhgan Moghanian</title>
 		<link rel="stylesheet" href="a1.css.css" type="text/css">
